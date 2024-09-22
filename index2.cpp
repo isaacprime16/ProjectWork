eadfbfe27bc0adf80a6bf3f76c31eae57357d9dc
Isaac Munene <isaac.munene@strathmore.edu>
2024-09-22 10:07:48 +0300

    Code edited to display list of the teams

--- index2.cpp
+++ index2.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
-
 using namespace std;
 
 // Defines structure for team
@@ -19,11 +18,12 @@ vector<FootballTeam> getTeamsFromCSV(const string& filename) {
     ifstream file(filename);
 
     if (!file.is_open()) {
-        cerr << "Oops! Could not open the file: " << filename << endl;
+        cerr << "Error: Could not open file " << filename << endl;
         return teams;
     }
 
     string line;
+
     // Skip the header line
     getline(file, line);
 
@@ -45,21 +45,26 @@ vector<FootballTeam> getTeamsFromCSV(const string& filename) {
     return teams;
 }
 
+
+
 int main() {
     // Set the file path to your CSV file
-    string filename = R"(C:\CSV Match fixtures\teams_csvFile.csv)";
+    string filename = "Teams.csv";
     // Get the teams from the CSV file
     vector<FootballTeam> teams = getTeamsFromCSV(filename);
 
     // Output the list of teams
-    cout << "Here are some exciting football teams and where you can catch them in action!" << endl << endl;
-    for (const auto& team : teams) {
-        cout << "Introducing " << team.teamName << " from " << team.localTown << "!" << endl;
-        cout << "You can watch their thrilling matches at " << team.homeStadium << "." << endl;
-        cout << "----------------------------------------------------------" << endl;
+   if (!teams.empty()) {
+        cout << "Here are some exciting football teams and where you can catch them in action!" << endl << endl;
+        for (const auto& team : teams) {
+            cout << "Introducing " << team.teamName << " from " << team.localTown << "!" << endl;
+            cout << "You can watch their thrilling matches at " << team.homeStadium << "." << endl;
+            cout << "----------------------------------------------------------" << endl;
+        }
+        cout << endl << "That's the list of teams! Stay tuned for more football action!" << endl;
+    } else {
+        cout << "No teams were loaded. Please check your CSV file." << endl;
     }
-
-    cout << endl << "That's the list of teams! Stay tuned for more football action!" << endl;
 
     return 0;
 }
